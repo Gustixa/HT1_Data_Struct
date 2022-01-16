9aeafefa106c7915b1a472a8e36d023452b5a0ce
reddubois <54864123+reddubois@users.noreply.github.com>
2022-01-16 15:51:52 -0600

    Interfaz y clase radio aún sin editar

---
+++ Radio.java
@@ -0,0 +1,24 @@
+package com.uvg.ayed;
+
+/*************************************************
+ * Clase radio (Radio.java)
+ * Enero del 2022
+ * Controla el estado y acciones en la radio
+ * Importa el paquete completo java.util
+ * @author Rafael Dubois 19093
+ * @author Josue Argueta 211024
+**************************************************/
+
+import java.util.*;
+
+public class Radio implements IRadio{
+
+	/** Declarar atributos*/
+	
+	
+	
+	/** Implementar métodos*/
+	
+	
+	
+}
